07bea9b9b7bb468afbfb10d05c5cdbd871c7aebb
Suman Kumar <skcool.123bgp@gmail.com>
2020-05-03 03:24:47 +0530

    :zap: fix link in notepad

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,62 +1,63 @@
-import * as React from "react";
+import * as React from 'react';
 import '../styles/notepad.css';
-import { Dialog, DialogTitle, TextField, Button } from "@material-ui/core";
+import { Dialog, DialogTitle, TextField, Button } from '@material-ui/core';
 export class Table extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
       toggleDialog: false,
-      linkName: "",
-      linkURL: "",
+      linkName: '',
+      linkURL: '',
     };
   }
   createLink = () => {
-    const anchorTag = document.createElement("a");
-    anchorTag.setAttribute("href", this.state.linkURL);
+    const anchorTag = document.createElement('a');
+    anchorTag.setAttribute('href', this.state.linkURL);
     const textNode = document.createTextNode(this.state.linkName);
     anchorTag.appendChild(textNode);
-    const div = document.createElement("div");
-    div.setAttribute("contenteditable", "false");
+    const div = document.createElement('div');
+    div.setAttribute('contenteditable', 'false');
+    div.style.display = 'inline';
     div.appendChild(anchorTag);
-    const notepadId = document.getElementById("table");
+    const notepadId = document.getElementById('table');
     notepadId?.appendChild(div);
-  };
+  }
 
   insertLinkDialog = () => (
     <Dialog
       onClose={this.toggleDialog}
       open={this.state.toggleDialog}
-      className={"dialog"}
+      className={'dialog'}
     >
       <DialogTitle
-        style={{ display: "flex", justifyContent: "center", padding: "10px" }}
+        style={{ display: 'flex', justifyContent: 'center', padding: '10px' }}
       >
         Insert Link
       </DialogTitle>
       <TextField
-        style={{ width: "80px", padding: "10px" }}
-        placeholder={"Name:"}
+        style={{ width: '80px', padding: '10px' }}
+        placeholder={'Name:'}
         onChange={(e) => {
           this.setState({ linkName: e.target.value });
         }}
       ></TextField>
       <TextField
-        style={{ padding: "10px" }}
-        placeholder={"https://www.example.com"}
+        style={{ padding: '10px' }}
+        placeholder={'https://www.example.com'}
         onChange={(e) => {
           this.setState({ linkURL: e.target.value });
         }}
       ></TextField>
       <Button
         style={{
-          textTransform: "capitalize",
-          borderRadius: "0px",
-          display: "block",
-          margin: "auto",
-          marginBottom: "6px",
+          textTransform: 'capitalize',
+          borderRadius: '0px',
+          display: 'block',
+          margin: 'auto',
+          marginBottom: '6px',
         }}
-        variant="contained"
-        color="primary"
+        variant='contained'
+        color='primary'
         onClick={() => {
           this.toggleDialog();
           this.createLink();
@@ -65,11 +66,11 @@ export class Table extends React.Component<Props, State> {
         Insert
       </Button>
     </Dialog>
-  );
+  )
 
   toggleDialog = () => {
     this.setState({ toggleDialog: !this.state.toggleDialog });
-  };
+  }
   render() {
     return (
       /**
@@ -84,15 +85,15 @@ export class Table extends React.Component<Props, State> {
       //     </tbody>
       //   </table>
       <>
-        <div contentEditable={true} id="table"></div>
+        <div contentEditable={true} id='table'></div>
         <Button
-          variant="contained"
-          className="card"
+          variant='contained'
+          className='card'
           onClick={this.toggleDialog}
           style={{
-            borderRadius: "0px",
-            background: "white",
-            textTransform: "capitalize",
+            borderRadius: '0px',
+            background: 'white',
+            textTransform: 'capitalize',
           }}
         >
           Link
